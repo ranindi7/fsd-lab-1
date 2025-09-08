621ea003fe720c8f1667a7448566eb5d1926b11a
Ranindi Gunasekera <ngunasekera3@academic.rrc.ca>
2025-09-07 23:25:34 -0500

    Created displayEmployees function to create and populate elements to display employee data

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
     const currentYear = document.getElementById("year");
     currentYear.textContent = `${new Date().getFullYear()}`
+});
 
-    
-})
+function displayEmployees(departmentData) {
+    const main = document.querySelector("main");
+    const section = document.createElement("section");
+
+    departmentData.forEach(departments => {
+        const h4 = document.createElement("h4");
+        h4.textContent = departments.department;
+        section.appendChild(h4);
+
+        const ul = document.createElement("ul");
+        departments.employees.forEach(employee => {
+            const li = document.createElement("li");
+            li.textContent = employee;
+            ul.appendChild(li);
+        });
+
+        section.appendChild(ul);
+    });
+
+    main.appendChild(section);
+};
