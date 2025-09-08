14c4ed6795939e26058acb12ac953590aba021d1
Ranindi Gunasekera <ngunasekera3@academic.rrc.ca>
2025-09-07 20:46:52 -0500

    Created json file with all employees categorized by departments

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
     const currentYear = document.getElementById("year");
     currentYear.textContent = `${new Date().getFullYear()}`
+
+    
 })
