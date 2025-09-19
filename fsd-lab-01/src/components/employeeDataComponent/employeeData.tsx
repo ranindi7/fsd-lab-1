144cc884b4b2c84a479a4fe6a22bbf44cafc91c7
Ranindi Gunasekera <ngunasekera3@academic.rrc.ca>
2025-09-19 09:56:00 -0500

    Displayed employee data correctly

--- fsd-lab-01/src/components/employeeDataComponent/employeeData.tsx
+++ fsd-lab-01/src/components/employeeDataComponent/employeeData.tsx
@@ -1,3 +1,23 @@
-import employees from "../../employeeData/employees.json";
-import type { Employee } from "../../interface/Employee";
+import employeeData from "../../employeeData/employees.json";
+import type { Department } from "../../types/employeeInterface";
 
+export default function EmployeeData() {
+    return(
+        <main>
+            <h1>Employee Directory</h1>
+
+            <div>
+                {employeeData.map((dept: Department) => (
+                <div className="employeesDisplay">
+                    <h2>{dept.department}</h2>
+                    <ul>
+                        {dept.employees.map((emp) => (
+                            <li>{emp}</li>
+                        ))}
+                    </ul>
+                </div>
+                ))}
+            </div>
+        </main>
+    )
+}
