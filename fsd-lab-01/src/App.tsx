e88ce6af8244a092cad61540a28af9b28b7c7912
Ranindi Gunasekera <ngunasekera3@academic.rrc.ca>
2025-09-29 19:08:05 -0500

    Reset app.tsx to route correctly

--- fsd-lab-01/src/App.tsx
+++ fsd-lab-01/src/App.tsx
@@ -2,18 +2,14 @@ import './App.css'
 import Header from './components/headerComponent/Header'
 import Footer from './components/footerComponent/Footer'
 import EmployeeData from './components/pages/employeeData'
-import { OrganizationData } from './components/pages/organizationData'
-import organizationList from './jsonData/organization.json'
-import type { Organization } from './types/organizationInterface'
 
 function App() {
-  const organizations: Organization[] = organizationList
+
 
   return (
     <>
       <Header />
       <EmployeeData />
-      <OrganizationData organizationList={organizations} />
       <Footer />
     </>
   )
